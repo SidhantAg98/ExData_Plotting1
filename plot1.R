bcfb90d72668599beff2ac5cae558480c7347b12
SidhantAg98 <sidhant.ag98@gmail.com>
2020-02-20 20:33:34 +0530

    Histogram plot of Global Active Power

---
+++ plot1.R
@@ -0,0 +1,8 @@
+library(data.table)
+data <- read.table(file = "household_power_consumption.txt",header = TRUE,sep = ";",na.strings = "?")
+data$Date <- as.Date(data$Date,format = "%d/%m/%Y")
+data <- subset(data,Date >= "2007-02-01" & Date <= "2007-02-02")
+data$datetime <- strptime(paste(data$Date, data$Time), "%Y-%m-%d %H:%M:%S")
+hist(data$Global_active_power,col = "red",main = "Global Active Power",xlab = "Global Active Power (kilowatts)")
+dev.copy(device = png, file = "plot1.png", height = 480 , width = 480)
+dev.off()
